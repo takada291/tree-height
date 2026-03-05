4092752fe9cbab9237b55c59e712ec076cca1827
takada291 <tkp291@gmail.com>
2026-03-05 14:00:39 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tree-height-v1.1.1';
+const CACHE_NAME = 'tree-height-v1.2.0';
 const urlsToCache = [
   './',
   'index.html',
@@ -38,3 +38,4 @@ self.addEventListener('fetch', (event) => {
   );
 
 });
+
